c809f11c023ac69dd4ffca7cad8d0a4197a16008
Kenny Lövrin <kennylovrin@icloud.com>
2015-07-10 22:20:35 +0200

    working basic vimage

--- ColorCorrect.playground/Pages/vImage.xcplaygroundpage/Contents.swift
+++ ColorCorrect.playground/Pages/vImage.xcplaygroundpage/Contents.swift
@@ -13,12 +13,14 @@ let bitsPerPixel = CGImageGetBitsPerPixel(image?.backingCGImage())
 let colorSpace = CGImageGetColorSpace(image?.backingCGImage())!
 let bitmapInfo = CGImageGetBitmapInfo(image?.backingCGImage())
 
+print(bitmapInfo.contains(CGBitmapInfo.ByteOrderDefault))
+
 // create a format for input
 var inFormat = vImage_CGImageFormat(
-    bitsPerComponent: UInt32(bitsPerComponent),
-    bitsPerPixel: UInt32(bitsPerPixel),
+    bitsPerComponent: 32,
+    bitsPerPixel: 96,
     colorSpace: Unmanaged.passUnretained(colorSpace),
-    bitmapInfo: [CGBitmapInfo.FloatComponents, CGBitmapInfo.ByteOrder16Little],
+    bitmapInfo: [CGBitmapInfo.FloatComponents, CGBitmapInfo.ByteOrder32Little, CGBitmapInfo(rawValue: CGImageAlphaInfo.None.rawValue)],
     version: 0,
     decode: nil,
     renderingIntent: CGColorRenderingIntent.RenderingIntentDefault
@@ -46,9 +48,28 @@ var b = vImage_Buffer(data: &bData, height: vImagePixelCount(height), width: vIm
 
 vImageConvert_RGBFFFtoPlanarF(&inBuffer, &r, &g, &b, vImage_Flags(kvImageNoFlags))
 
+vImageContrastStretch_PlanarF(&r, &r, nil, 255, 0.2, 0.8, vImage_Flags(kvImageNoFlags))
+vImageContrastStretch_PlanarF(&g, &g, nil, 255, 0.2, 0.8, vImage_Flags(kvImageNoFlags))
+vImageContrastStretch_PlanarF(&b, &b, nil, 255, 0.2, 0.8, vImage_Flags(kvImageNoFlags))
+
+/*var buffers = [UnsafePointer<vImage_Buffer>]()
+buffers.append(&r)
+buffers.append(&g)
+buffers.append(&b)
+
+var matrix: [Float] = [
+    1, 0, 0,
+    0.4, 1, 0,
+    0.4, 0.8, 1
+]
+
+var bias: [Float] = [0, 0, 0]
+
+vImageMatrixMultiply_PlanarF(&buffers, &buffers, 3, 3, &matrix, &bias, &bias, vImage_Flags(kvImageNoFlags))*/
+
 var outFormat = inFormat
-outFormat.bitsPerComponent = 16
-outFormat.bitsPerPixel = 16
+outFormat.bitsPerComponent = 32
+outFormat.bitsPerPixel = 32
 outFormat.colorSpace = Unmanaged.passRetained(CGColorSpaceCreateDeviceGray()!)
 
 var outRCGImage = vImageCreateCGImageFromBuffer(&r, &outFormat, nil, nil, vImage_Flags(kvImageNoFlags), nil).takeRetainedValue()
@@ -61,3 +82,8 @@ var outBCGImage = vImageCreateCGImageFromBuffer(&b, &outFormat, nil, nil, vImage
 var outBImage = CIImage(CGImage: outBCGImage)
 
 // merge the three planes back together
+vImageConvert_PlanarFtoRGBFFF(&r, &g, &b, &inBuffer, vImage_Flags(kvImageNoFlags))
+
+// create output image (verifying it comes in and out properly)
+outCGImage = vImageCreateCGImageFromBuffer(&inBuffer, &inFormat, nil, nil, vImage_Flags(kvImageNoFlags), nil).takeRetainedValue()
+outImage = CIImage(CGImage: outCGImage)
